63eda4d45cd953ce64459b472818b35a81a891bb
Hector-1911 <fontas.chrome@gmail.com>
2019-12-06 17:32:55 +0300

    345

--- js/hotelsPopup.js
+++ js/hotelsPopup.js
@@ -6,3 +6,35 @@ link.addEventListener("click", function(evt) {
     evt.preventDefault();
     popup.classList.toggle("modal-show");
 });
+
+
+var adulsCounter = document.querySelector(".aduls-counter");
+var kidsCounter = document.querySelector(".kids-counter");
+var adulsMinus = document.querySelector(".aduls-minus-btn");
+var adulsPlus = document.querySelector(".aduls-plus-btn");
+var kidsMinus = document.querySelector(".kids-minus-btn");
+var kidsPlus = document.querySelector(".kids-plus-btn");
+
+
+adulsMinus.addEventListener("click", function(evt) {
+    evt.preventDefault();
+    if (adulsCounter.value > 0) {
+       adulsCounter.value = adulsCounter.value - 1;
+    }
+});
+adulsPlus.addEventListener("click", function(evt) {
+    evt.preventDefault();
+    adulsCounter.value = (adulsCounter.value - 1) + 2;
+});
+
+kidsMinus.addEventListener("click", function(evt) {
+    evt.preventDefault();
+    if (kidsCounter.value > 0) {
+       kidsCounter.value = kidsCounter.value - 1;
+    }
+});
+
+kidsPlus.addEventListener("click", function(evt) {
+    evt.preventDefault();
+    kidsCounter.value = (kidsCounter.value - 1) + 2;
+});
